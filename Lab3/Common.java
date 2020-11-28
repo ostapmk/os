85fc40913ce11625b6dd9606342acdc6e5fa77cb
ostapmk <ostapmk@gmail.com>
2020-11-28 18:03:05 +0200

    Run formatter to make this dumb code more readable

--- Lab3/Common.java
+++ Lab3/Common.java
@@ -1,6 +1,6 @@
 public class Common {
 
-  static public int s2i (String s) {
+  static public int s2i(String s) {
     int i = 0;
 
     try {
@@ -11,7 +11,7 @@ public class Common {
     return i;
   }
 
-  static public double R1 () {
+  static public double R1() {
     java.util.Random generator = new java.util.Random(System.currentTimeMillis());
     double U = generator.nextDouble();
     while (U < 0 || U >= 1) {
@@ -21,14 +21,20 @@ public class Common {
     while (V < 0 || V >= 1) {
       V = generator.nextDouble();
     }
-    double X =  Math.sqrt((8/Math.E)) * (V - 0.5)/U;
-    if (!(R2(X,U))) { return -1; }
-    if (!(R3(X,U))) { return -1; }
-    if (!(R4(X,U))) { return -1; }
+    double X = Math.sqrt((8 / Math.E)) * (V - 0.5) / U;
+    if (!(R2(X, U))) {
+      return -1;
+    }
+    if (!(R3(X, U))) {
+      return -1;
+    }
+    if (!(R4(X, U))) {
+      return -1;
+    }
     return X;
   }
 
-  static public boolean R2 (double X, double U) {
+  static public boolean R2(double X, double U) {
     if ((X * X) <= (5 - 4 * Math.exp(.25) * U)) {
       return true;
     } else {
@@ -36,7 +42,7 @@ public class Common {
     }
   }
 
-  static public boolean R3 (double X, double U) {
+  static public boolean R3(double X, double U) {
     if ((X * X) >= (4 * Math.exp(-1.35) / U + 1.4)) {
       return false;
     } else {
@@ -44,7 +50,7 @@ public class Common {
     }
   }
 
-  static public boolean R4 (double X, double U) {
+  static public boolean R4(double X, double U) {
     if ((X * X) < (-4 * Math.log(U))) {
       return true;
     } else {
@@ -53,4 +59,3 @@ public class Common {
   }
 
 }
-
--- Lab3/Scheduling.java
+++ Lab3/Scheduling.java
@@ -7,12 +7,14 @@
 
 // Created by Alexander Reeder, 2001 January 06
 
-import java.io.*;
-import java.util.*;
-import sProcess;
-import Common;
-import Results;
-import SchedulingAlgorithm;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.StringTokenizer;
+import java.util.Vector;
 
 public class Scheduling {
 
@@ -20,21 +22,19 @@ public class Scheduling {
   private static int meanDev = 1000;
   private static int standardDev = 100;
   private static int runtime = 1000;
-  private static Vector processVector = new Vector();
-  private static Results result = new Results("null","null",0);
+  private static Vector<Process> processVector = new Vector<Process>();
+  private static Results result = new Results("null", "null", 0);
   private static String resultsFile = "Summary-Results";
 
-  private static void Init(String file) {
+  private static void init(String file) {
     File f = new File(file);
     String line;
-    String tmp;
     int cputime = 0;
     int ioblocking = 0;
     double X = 0.0;
 
-    try {   
-      //BufferedReader in = new BufferedReader(new FileReader(f));
-      DataInputStream in = new DataInputStream(new FileInputStream(f));
+    try {
+      var in = new BufferedReader(new FileReader(f));
       while ((line = in.readLine()) != null) {
         if (line.startsWith("numprocess")) {
           StringTokenizer st = new StringTokenizer(line);
@@ -61,7 +61,7 @@ public class Scheduling {
           }
           X = X * standardDev;
           cputime = (int) X + meanDev;
-          processVector.addElement(new sProcess(cputime, ioblocking, 0, 0, 0));          
+          processVector.addElement(new Process(cputime, ioblocking, 0, 0, 0));
         }
         if (line.startsWith("runtime")) {
           StringTokenizer st = new StringTokenizer(line);
@@ -70,21 +70,8 @@ public class Scheduling {
         }
       }
       in.close();
-    } catch (IOException e) { /* Handle exceptions */ }
-  }
-
-  private static void debug() {
-    int i = 0;
-
-    System.out.println("processnum " + processnum);
-    System.out.println("meandevm " + meanDev);
-    System.out.println("standdev " + standardDev);
-    int size = processVector.size();
-    for (i = 0; i < size; i++) {
-      sProcess process = (sProcess) processVector.elementAt(i);
-      System.out.println("process " + i + " " + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.numblocked);
-    }
-    System.out.println("runtime " + runtime);
+    } catch (IOException e) {
+      /* Handle exceptions */ }
   }
 
   public static void main(String[] args) {
@@ -94,34 +81,32 @@ public class Scheduling {
       System.out.println("Usage: 'java Scheduling <INIT FILE>'");
       System.exit(-1);
     }
-    File f = new File(args[0]);
+    var f = new File(args[0]);
     if (!(f.exists())) {
       System.out.println("Scheduling: error, file '" + f.getName() + "' does not exist.");
       System.exit(-1);
-    }  
+    }
     if (!(f.canRead())) {
       System.out.println("Scheduling: error, read of " + f.getName() + " failed.");
       System.exit(-1);
     }
     System.out.println("Working...");
-    Init(args[0]);
+    init(args[0]);
     if (processVector.size() < processnum) {
       i = 0;
-      while (processVector.size() < processnum) {       
-          double X = Common.R1();
-          while (X == -1.0) {
-            X = Common.R1();
-          }
-          X = X * standardDev;
+      while (processVector.size() < processnum) {
+        double X = Common.R1();
+        while (X == -1.0) {
+          X = Common.R1();
+        }
+        X = X * standardDev;
         int cputime = (int) X + meanDev;
-        processVector.addElement(new sProcess(cputime,i*100,0,0,0));          
+        processVector.addElement(new Process(cputime, i * 100, 0, 0, 0));
         i++;
       }
     }
-    result = SchedulingAlgorithm.Run(runtime, processVector, result);    
-    try {
-      //BufferedWriter out = new BufferedWriter(new FileWriter(resultsFile));
-      PrintStream out = new PrintStream(new FileOutputStream(resultsFile));
+    result = SchedulingAlgorithm.run(runtime, processVector, result);
+    try (var out = new PrintStream(new FileOutputStream(resultsFile));) {
       out.println("Scheduling Type: " + result.schedulingType);
       out.println("Scheduling Name: " + result.schedulingName);
       out.println("Simulation Run Time: " + result.compuTime);
@@ -129,20 +114,36 @@ public class Scheduling {
       out.println("Standard Deviation: " + standardDev);
       out.println("Process #\tCPU Time\tIO Blocking\tCPU Completed\tCPU Blocked");
       for (i = 0; i < processVector.size(); i++) {
-        sProcess process = (sProcess) processVector.elementAt(i);
+        var process = processVector.elementAt(i);
         out.print(Integer.toString(i));
-        if (i < 100) { out.print("\t\t"); } else { out.print("\t"); }
+        if (i < 100) {
+          out.print("\t\t");
+        } else {
+          out.print("\t");
+        }
         out.print(Integer.toString(process.cputime));
-        if (process.cputime < 100) { out.print(" (ms)\t\t"); } else { out.print(" (ms)\t"); }
+        if (process.cputime < 100) {
+          out.print(" (ms)\t\t");
+        } else {
+          out.print(" (ms)\t");
+        }
         out.print(Integer.toString(process.ioblocking));
-        if (process.ioblocking < 100) { out.print(" (ms)\t\t"); } else { out.print(" (ms)\t"); }
+        if (process.ioblocking < 100) {
+          out.print(" (ms)\t\t");
+        } else {
+          out.print(" (ms)\t");
+        }
         out.print(Integer.toString(process.cpudone));
-        if (process.cpudone < 100) { out.print(" (ms)\t\t"); } else { out.print(" (ms)\t"); }
+        if (process.cpudone < 100) {
+          out.print(" (ms)\t\t");
+        } else {
+          out.print(" (ms)\t");
+        }
         out.println(process.numblocked + " times");
       }
-      out.close();
-    } catch (IOException e) { /* Handle exceptions */ }
-  System.out.println("Completed.");
+    } catch (IOException e) {
+      /* Handle exceptions */ 
+    }
+    System.out.println("Completed.");
   }
 }
-
